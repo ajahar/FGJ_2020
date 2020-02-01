1af5b0697f875a4a7c9a782b19613b57bb0ff5f3
Antti Harju <antti.harju@saunalahti.fi>
2020-02-01 20:20:25 +0200

    Stop ControllableBlock on contact

--- Blocks/ControllableBlock.gd
+++ Blocks/ControllableBlock.gd
@@ -3,12 +3,23 @@ extends RigidBody2D
 export var speed = 3  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
 
+var controllable = true
+
 func _ready():
 	screen_size = get_viewport_rect().size
 	
 func _integrate_forces(state):
+	if !controllable:
+		return
+		
 	if Input.is_action_pressed("ui_left"):
 		state.apply_central_impulse(Vector2.LEFT * speed)
 	if Input.is_action_pressed("ui_right"):
 		state.apply_central_impulse(Vector2.RIGHT * speed)
 	# TODO: Clamp?
+
+
+func on_body_entered(body):
+	if controllable:
+		mode = RigidBody2D.MODE_KINEMATIC
+		controllable = false
