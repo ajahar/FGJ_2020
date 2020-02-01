da867fb4457abc82b2bea117858c72e55f780728
Antti Harju <antti.harju@saunalahti.fi>
2020-02-01 19:54:03 +0200

    Functional ControllableBlock inputs using physics impulse

--- Blocks/ControllableBlock.gd
+++ Blocks/ControllableBlock.gd
@@ -1,29 +1,14 @@
 extends RigidBody2D
 
-export var speed = 300  # How fast the player will move (pixels/sec).
+export var speed = 3  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
-
-func _physics_process(delta):
-	var velocity = Vector2()  # The player's movement vector.
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _integrate_forces(state):
+	if Input.is_action_pressed("ui_left"):
+		state.apply_central_impulse(Vector2.LEFT * speed)
+	if Input.is_action_pressed("ui_right"):
+		state.apply_central_impulse(Vector2.RIGHT * speed)
+	# TODO: Clamp?
