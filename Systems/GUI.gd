80cd54edcc5ec82529d806b0ee235b2375863158
Antti Harju <antti.harju@saunalahti.fi>
2020-02-02 16:36:14 +0200

    Add UI for game end

--- Systems/GUI.gd
+++ Systems/GUI.gd
@@ -1,8 +1,9 @@
 extends MarginContainer
 
-onready var mushrooms: Label = get_node("VBoxContainer/Mushrooms")
-onready var play_time: Label = get_node("VBoxContainer/PlayTime")
-onready var message: Label = get_node("VBoxContainer/Message")
+onready var mushrooms: Label = get_node("Panel/Mushrooms")
+onready var play_time: Label = get_node("Panel/PlayTime")
+onready var message: Label = get_node("Panel/Message")
+onready var end_panel: CanvasItem = get_node("Panel/End")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,3 +20,12 @@ func on_use_mushroom():
 
 func on_update_message(signal_text):
 	message.set_text(signal_text)
+	end_panel.visible = true
+	
+
+func _on_Restart_pressed():
+	get_tree().change_scene("res://Menu.tscn")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
